9a1abba41770b85c522cec21ed950fdbdc8a1815
Natharuch <natharuch@newsomthai.net>
2023-04-24 11:36:54 +0700

    er

--- Client/src/main.ts
+++ Client/src/main.ts
@@ -1,5 +1,7 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {  } from '@angular-devkit/build-angular';
+
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment.prod';
